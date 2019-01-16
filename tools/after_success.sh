07ed7c7aeb9901c0671518090538b65aaecefaff
Simon Berger <siku2@outlook.com>
2019-01-16 01:40:32 +0100

    dredd can quack off for now...

--- tools/after_success.sh
+++ tools/after_success.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
 if [[ ${TRAVIS_PULL_REQUEST} == "false" ]] && [[ ${TRAVIS_BRANCH} == "master" ]]; then
-    tools/deploy.sh
-    npx dredd init -r apiary -j apiaryApiKey:${APIARY_API_KEY} -j apiaryApiName:${APIARY_API_NAME}
-    npx dredd
+    bash tools/deploy.sh
 fi
